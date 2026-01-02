669adc3c6c60f8f43a2d4e26840eba045c00409c
Rain <rainkalugdan@gmail.com>
2026-01-02 19:02:03 +0800

    feat(home): add Home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
-  return <h1>Asian Women Tech</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
