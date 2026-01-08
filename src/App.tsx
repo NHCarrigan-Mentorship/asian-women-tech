141d94dc617b6a02c64452e882e7d12c448efbb9
Rain <rainkalugdan@gmail.com>
2026-01-08 12:57:00 +0800

    feat(layout): render layout in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Navigation from "./components/Navigation";
+import Layout from "./components/Layout";
 import "./styles/App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Navigation />}>
+        <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Route>
