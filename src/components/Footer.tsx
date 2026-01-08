538e2e25d6faee720aabdc7fc641886c0101f291
Rain <rainkalugdan@gmail.com>
2026-01-08 12:35:47 +0800

    feat(footer): add footer structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,10 @@
 export default function Footer() {
-  return <footer>Footer here</footer>;
+  return (
+    <footer className="flex flex-col py-8 gap-2 bg-gradient-to-r from-pink-500 to-rose-500 text-white">
+      <p className="font-bold text-center">© 2026 PinkTech</p>
+      <p className="text-sm text-center">
+        Celebrating and amplifying the voices of women in technology
+      </p>
+    </footer>
+  );
 }
