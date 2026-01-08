63f3da0ddddfa0ec76bca9315d59a6b0efa2fc35
Rain <rainkalugdan@gmail.com>
2026-01-08 12:55:36 +0800

    refactor(home): remove extra backticks

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <Hero />``
+      <Hero />
       {!isAuthenticated && <ProfileCreationAnimation />}
       <FeaturedProfiles />
     </div>
