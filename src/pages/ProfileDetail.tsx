d814079f74b20b78703f4e1f16df5970bc7a4c8b
Rain <rainkalugdan@gmail.com>
2026-01-10 14:53:49 +0800

    feat(profile): center profile not found

--- src/pages/ProfileDetail.tsx
+++ src/pages/ProfileDetail.tsx
@@ -1,17 +1,24 @@
 import { useParams } from "react-router-dom";
-import { profiles } from "../data/profiles";
+import ProfileNotFound from "../components/ProfileNotFound";
 import BackNavigation from "../components/BackNavigation";
 import ProfileAuthorshipNotice from "../components/ProfileAuthorshipNotice";
+import { profiles } from "../data/profiles";
 
 export default function ProfileDetail() {
   const { id } = useParams();
+  const profile = profiles.find((profile) => profile.id === id);
 
+  if (!profile) {
+    return (
+      <div className="flex flex-1 justify-center items-center p-10 bg-gray-50">
+        <ProfileNotFound />
+      </div>
+    );
+  }
   return (
-    <div className="min-h-screen bg-white">
+    <div className="bg-white">
       <BackNavigation />
       <ProfileAuthorshipNotice />
-      {/* Render profile */}
-      <h1>Dr. Amara Johnson</h1>
     </div>
   );
 }
