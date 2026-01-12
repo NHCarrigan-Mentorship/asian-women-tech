14a548331993b63b5d2da74d94df109b46880217
Rain <rainkalugdan@gmail.com>
2026-01-12 18:45:23 +0800

    feat(filtered-profiles): get list

--- src/pages/main/Discover.tsx
+++ src/pages/main/Discover.tsx
@@ -1,16 +1,31 @@
 import DiscoverHeader from "../../components/layout/DiscoverHeader";
+import FilteredProfiles from "../../components/profile/FilteredProfiles";
 import ProfileSearchBar from "../../components/profile/ProfileSearchBar";
 import { useState } from "react";
+import { profiles } from "../../data/profiles";
 
 export default function Discover() {
   const [search, setSearch] = useState("");
+  const filteredProfiles = profiles.filter(
+    (profile) =>
+      profile.name.toLowerCase().includes(search.toLowerCase()) ||
+      profile.role.toLowerCase().includes(search.toLowerCase()) ||
+      profile.company.toLowerCase().includes(search.toLowerCase())
+  );
+
+  console.log(filteredProfiles);
 
   const onSearch = setSearch;
 
   return (
     <>
       <DiscoverHeader />
-      <ProfileSearchBar search={search} onSearch={onSearch} />
+      <ProfileSearchBar
+        search={search}
+        onSearch={onSearch}
+        profilesCount={filteredProfiles.length}
+      />
+      <FilteredProfiles filteredProfiles={filteredProfiles} />
     </>
   );
 }
