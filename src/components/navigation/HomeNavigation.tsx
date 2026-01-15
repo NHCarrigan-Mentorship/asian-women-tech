77a6fd89137acb92968d4fa4244214d190dfffd0
Rain <rainkalugdan@gmail.com>
2026-01-15 12:48:55 +0800

    feat(home-nav): remove on close feature

--- src/components/navigation/HomeNavigation.tsx
+++ src/components/navigation/HomeNavigation.tsx
@@ -1,16 +1,11 @@
 import { Link } from "react-router-dom";
 import Logo from "/favicon.svg";
 
-interface HomeNavigationProps {
-  onClose: () => void;
-}
-
-export default function HomeNavigation({ onClose }: HomeNavigationProps) {
+export default function HomeNavigation() {
   return (
     <Link
       to="/"
       className="flex items-center gap-2 hover:opacity-80 transition-opacity cursor-pointer"
-      onClick={onClose}
     >
       <img src={Logo} alt="PinkTech Logo" className="h-8 w-8 md:h-10 md:w-10" />
       <span className="text-lg md:text-xl font-bold text-gray-900">
