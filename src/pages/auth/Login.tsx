d48892c49da587ab5c67df78ccfd618ff60dc565
Rain <rainkalugdan@gmail.com>
2026-01-17 18:52:43 +0800

    feat(login): add structure

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,3 +1,15 @@
+import AuthBackNavigation from "../../components/navigation/AuthBackNavigation";
+import AuthCard from "../../components/auth/AuthCard";
+
 export default function Login() {
-  return <h2>Login</h2>;
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-pink-50 via-pink-100 to-rose-50">
+      <div className="flex-1 flex justify-center items-center px-4 py-12">
+        <div className="w-full max-w-md">
+          <AuthBackNavigation />
+          <AuthCard type="login" />
+        </div>
+      </div>
+    </div>
+  );
 }
