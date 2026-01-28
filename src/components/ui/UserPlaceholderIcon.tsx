d565a4465050f48c5d4a4dc46e545d651b827ff9
Rain <rainkalugdan@gmail.com>
2026-01-28 11:43:38 +0800

    feat(ui): add user placeholder icon

---
+++ src/components/ui/UserPlaceholderIcon.tsx
@@ -0,0 +1,19 @@
+export default function UserPlaceholderIcon() {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 200 200"
+        width="100"
+        height="100"
+      >
+        <rect width="200" height="200" fill="#fce7f3" />
+        <circle cx="100" cy="75" r="35" fill="#ec4899" />
+        <path
+          d="M 100 120 C 65 120 40 145 40 170 L 40 200 L 160 200 L 160 170 C 160 145 135 120 100 120 Z"
+          fill="#ec4899"
+        />
+      </svg>
+    </div>
+  );
+}
