68200e0b359f9c95ce99316a7e3029ecdb09a467
Rain <rainkalugdan@gmail.com>
2026-01-28 16:35:57 +0800

    feat(use-profiles): fetch profiles from supabase

---
+++ src/hooks/useProfiles.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import supabase from "../config/supabaseClient";
+import type { UserProfile } from "../types/UserProfile";
+
+export default function useProfiles() {
+  const [profiles, setProfiles] = useState<UserProfile[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  // Get profiles from Supabase
+  useEffect(() => {
+    async function getProfiles() {
+      setLoading(true);
+      try {
+        const { data, error } = await supabase.from("profiles").select("*");
+        if (error) {
+          setError(error);
+          console.error(error);
+        } else {
+          setProfiles(data);
+        }
+      } catch (err) {
+        setError(err as Error);
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getProfiles();
+  }, []);
+
+  return { profiles, loading, error };
+}
