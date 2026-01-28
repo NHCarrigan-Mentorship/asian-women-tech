3224c8aca315937dcd86fd48986661e9c7310ffb
Rain <rainkalugdan@gmail.com>
2026-01-28 11:48:09 +0800

    feat(ui): move error image to ui

--- src/components/ui/ImageWithFallback.tsx
+++ src/components/ui/ImageWithFallback.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
+import ERROR_IMG_SRC from "./ErrorImage";
 import UserPlaceholderIcon from "./UserPlaceholderIcon";
-
-const ERROR_IMG_SRC =
-  "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iODgiIGhlaWdodD0iODgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgc3Ryb2tlPSIjMDAwIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBvcGFjaXR5PSIuMyIgZmlsbD0ibm9uZSIgc3Ryb2tlLXdpZHRoPSIzLjciPjxyZWN0IHg9IjE2IiB5PSIxNiIgd2lkdGg9IjU2IiBoZWlnaHQ9IjU2IiByeD0iNiIgZmlsbD0iI2ZjZTdmMyIvPjxwYXRoIGQ9Im0xNiA1OCAxNi0xOCAzMiAzMiIvPjxjaXJjbGUgY3g9IjUzIiBjeT0iMzUiIHI9IjciLz48L3N2Zz4K";
 
 export default function ImageWithFallback(
   props: React.ImgHTMLAttributes<HTMLImageElement>,
