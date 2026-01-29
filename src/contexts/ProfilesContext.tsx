157f4ebc4dd408946521fd8b69dfc85bc6f799d4
Rain <rainkalugdan@gmail.com>
2026-01-29 14:56:27 +0800

    feat(profiles-context): add context

---
+++ src/contexts/ProfilesContext.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext } from "react";
+import type { UserProfile } from "../types/UserProfile";
+import useProfiles from "../hooks/useProfiles";
+
+type ProfilesContextType = {
+  profiles: UserProfile[];
+  loading: boolean;
+  error: Error | null;
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const ProfilesContext = createContext<ProfilesContextType | undefined>(
+  undefined,
+);
+
+export function ProfilesProvider({ children }: Props) {
+  const { profiles, loading, error } = useProfiles();
+  return (
+    <ProfilesContext.Provider value={{ profiles, loading, error }}>
+      {children}
+    </ProfilesContext.Provider>
+  );
+}
+
+export function useProfilesContext() {
+  const context = useContext(ProfilesContext);
+  if (!context) {
+    throw new Error(
+      "useProfilesContext must be used within a ProfilesProvider",
+    );
+  } else {
+    return context;
+  }
+}
